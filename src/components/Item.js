c04b2ec9686822ccff4c7e4cc2f9a3bd1576201e
Roger Montes <rogerxmontes@gmail.com>
2023-06-22 10:08:32 -0600

    proyecto terminado

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+export default function Item({ item, onDeleteItems, onPackedItems }) {
+  return (
+    <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onPackedItems(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItems(item.id)}>❌</button>
+    </li>
+  );
+}
